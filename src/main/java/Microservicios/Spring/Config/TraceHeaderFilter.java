a9997016066d83eb962eb6dd370f7fb58c176317
Royer0528 <garciavazquezrogelio3@gmail.com>
2026-07-26 22:05:13 -0600

    Se agrega componente filter

---
+++ src/main/java/Microservicios/Spring/Config/TraceHeaderFilter.java
@@ -0,0 +1,38 @@
+package Microservicios.Spring.Config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class TraceHeaderFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String traceParent = request.getHeader("traceparent");
+        String traceState = request.getHeader("tracestate");
+
+        if (traceParent == null || traceParent.isBlank()) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                    "Header traceparent is required");
+            return;
+        }
+
+        if (traceState == null || traceState.isBlank()) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                    "Header tracestate is required");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
